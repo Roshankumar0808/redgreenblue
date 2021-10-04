0e2b968c6675c3bc74add2390ec8ace7b13e774e
Roshankumar0808 <roshananand2014@gmail.com>
2021-10-04 14:30:59 +0530

    feature: fitsrt push

---
+++ app.js
@@ -0,0 +1,11 @@
+const write = document.querySelector("#text");
+const first = document.querySelector("#r1");
+const second =document.querySelector("#g2");
+const third = document.querySelector("#b3");
+
+
+first.addEventListener("click",add)
+function add(){
+    
+    write.style.color= "red";
+}
